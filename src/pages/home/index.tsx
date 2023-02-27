11116b3611fe2a6ff15e297cb091964031513499
Daniel Cazé <danielctlima@gmail.com>
2023-02-27 12:37:15 -0300

    :lipstick: feat: Home page

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,31 @@
+import { Heading, Text } from '@ignite-ui/react'
+import { Hero, HomeContainer, Preview } from './styles'
+
+import previewImage from '../../assets/app-preview.png'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <HomeContainer>
+      <Hero>
+        <Heading as="h1" size="4xl">
+          Agendamento descomplicado
+        </Heading>
+        <Text size="xl">
+          Conecte seu calendario e permita que as pessoas marquem agendamentos
+          no seu tempo livre
+        </Text>
+      </Hero>
+
+      <Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          priority
+          alt="Calendario simbolizando aplicação em funcionamento"
+        />
+      </Preview>
+    </HomeContainer>
+  )
+}
