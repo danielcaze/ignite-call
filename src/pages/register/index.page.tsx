23ba1541e675f011508350f05e9653e5361f43e9
Daniel Cazé <danielctlima@gmail.com>
2023-02-28 09:36:07 -0300

    :hammer: feat: Username with query params

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,6 +1,8 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+import { useRouter } from 'next/router'
 import { ArrowRight } from 'phosphor-react'
+import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { Container, Form, FormError, Header } from './styles'
@@ -24,12 +26,20 @@ export default function Register() {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors, isSubmitting },
   } = useForm<RegisterFormType>({
     resolver: zodResolver(RegisterFormSchema),
   })
+  const router = useRouter()
 
-  async function handleRegister(data: RegisterFormType) { }
+  useEffect(() => {
+    if (router.query.username) {
+      setValue('username', String(router.query.username))
+    }
+  }, [router.query?.username, setValue])
+
+  async function handleRegister(data: RegisterFormType) {}
 
   return (
     <Container>
