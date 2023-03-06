36aafbf646a4e0eb2ef7017086642909fba77a84
Daniel Cazé <danielctlima@gmail.com>
2023-03-06 13:05:49 -0300

    :wrench: feat: Prisma custom adapter

---
+++ src/@types/next-auth.d.ts
@@ -0,0 +1,12 @@
+import NextAuth from 'next-auth'
+// PRECISA IMPORTAR PRA ELE SABER QUE ESTAMOS SOBRESCREVENDO NAO CRIANDO DO ZERO
+
+declare module 'next-auth' {
+  interface User {
+    id: string,
+    name: string,
+    email: string
+    username: string
+    avatar_url: string
+  }
+}
