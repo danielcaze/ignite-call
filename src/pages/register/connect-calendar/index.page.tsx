b571528745b5f3303511ce45c73880f926a52a2a
Daniel Cazé <danielctlima@gmail.com>
2023-03-13 12:15:03 -0300

    :hammer: feat: disponibility page

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -5,7 +5,7 @@ import { ArrowRight, Check } from 'phosphor-react'
 import { Container, Header } from '../styles'
 import { AuthError, ConnectBox, ConnectItem } from './styles'
 
-export default function RegisterCalendar() {
+export default function ConnectCalendar() {
   const session = useSession()
   const router = useRouter()
   const hasAuthError = !!router.query.error
