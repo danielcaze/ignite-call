9d483a63ea8b31a5adb07735cccf2b9b659c5abe
Daniel Cazé <danielctlima@gmail.com>
2023-05-25 15:17:56 -0300

    :hammer: feat: mysql and remove full dates

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -26,6 +26,7 @@ type CalendarWeeks = CalendarWeek[];
 
 interface BlockedDates {
   blockedWeekDays: number[];
+  blockedDates: number[];
 }
 
 interface CalendarProps {
@@ -50,15 +51,19 @@ export function Calendar({ onDateSelected, selectedDate }: CalendarProps) {
       const response = await api.get(`users/${username}/blocked-dates`, {
         params: {
           year: currentDate.get("year"),
-          month: currentDate.get("month"),
+          month: String(currentDate.get("month") + 1).padStart(2, "0"),
         },
       });
 
       return response.data;
     }
   );
-
+  console.log(blockedDates);
   const calendarWeeks = useMemo(() => {
+    if (!blockedDates) {
+      return [];
+    }
+
     const daysInMonthArray = Array.from({
       length: currentDate.daysInMonth(),
     }).map((_, i) => {
@@ -95,7 +100,8 @@ export function Calendar({ onDateSelected, selectedDate }: CalendarProps) {
           date,
           disabled:
             date.endOf("day").isBefore(new Date()) ||
-            blockedDates?.blockedWeekDays.includes(date.get("day")),
+            blockedDates.blockedWeekDays.includes(date.get("day")) ||
+            blockedDates.blockedDates.includes(date.get("date")),
         };
       }),
       ...nextMonthFillArray.map((date) => {
